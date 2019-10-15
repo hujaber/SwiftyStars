2a584b221e05a2fd5f0f547d92041025b9be09df
Hussein Jaber <hujaber@me.com>
2019-10-15 14:31:56 +0300

    Initial Commit

---
+++ SwiftyStars/SwiftyStars.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyStars.h
+//  SwiftyStars
+//
+//  Created by Hussein Jaber on 15/10/19.
+//  Copyright © 2019 Hussein Jaber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyStars.
+FOUNDATION_EXPORT double SwiftyStarsVersionNumber;
+
+//! Project version string for SwiftyStars.
+FOUNDATION_EXPORT const unsigned char SwiftyStarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyStars/PublicHeader.h>
+
+
